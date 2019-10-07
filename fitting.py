7968884277db34b32ac21d08a7a5c6a8c525c8a8
lichyyyyyy <44421466+lichyyyyyy@users.noreply.github.com>
2019-10-07 00:36:01 -0300

    Add files via upload

--- fitting.py
+++ fitting.py
@@ -1,7 +1,6 @@
 import pickle 
 import torch
 from torch.autograd import Variable
-import torch.nn.functional as F
 import torch.utils.data as Data
 import numpy as np
 from functools import reduce
@@ -9,6 +8,7 @@ import matplotlib.pyplot as plt
 import imageio
 import random
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 BATCH_SIZE = 64
@@ -16,16 +16,20 @@ EPOCH = 200
 TRAIN_DATASET = 0.9
 
 def basic(x, a, b, c, d):
-    a,b,c,d = convert2numpy([a,b,c,d])
-    return np.add(
+    a,b,c,d = param_constraints([a,b,c,d])
+    a = a.squeeze()
+    b = b.squeeze()
+    c = c.squeeze()
+    d = d.squeeze()
+    return torch.add(
         a,
-        np.divide(
+        torch.div(
             b,
-            np.add(
+            torch.add(
                 c,
-                np.exp(
-                    np.multiply(
-                        np.negative(d),
+                torch.exp(
+                    torch.mul(
+                        torch.neg(d),
                         x
                     )
                 )
@@ -35,20 +39,22 @@ def basic(x, a, b, c, d):
 
 
 def four_pl(x, a, b, c, d):
-    return np.add(
+    a,b,c,d = param_constraints([a,b,c,d])
+    return torch.add(
         c,
-        np.divide(
-            np.subtract(
-                d, c
+        torch.div(
+            torch.add(
+                d, 
+                torch.neg(c)
             ),
-            np.add(
+            torch.add(
                 1,
-                np.exp(
-                    np.multiply(
+                torch.exp(
+                    torch.mul(
                         a,
-                        np.subtract(
+                        torch.add(
                             x,
-                            b
+                            torch.neg(b)
                         )
                     )
                 )
@@ -58,6 +64,7 @@ def four_pl(x, a, b, c, d):
 
 
 def five_pl(x, a, b, c, d, g):
+    a,b,c,d,g = param_constraints([a,b,c,d,g])
     return np.add(
         c,
         np.divide(
@@ -140,55 +147,61 @@ def convert2numpy(params):
     return [param.detach().numpy() for param in params]
 
 
-class Net(torch.nn.Module):
-    def __init__(self, input_channel, output_channel, n_features, n_output, batch):
-        super(Net, self).__init__()
-        self.conv = torch.nn.Conv1d(input_channel, output_channel, kernel_size=3, stride=1, padding=1)  
-        self.pool = torch.nn.MaxPool1d(kernel_size=2, padding=0)
-        self.fc = torch.nn.Linear(int(n_features*batch*output_channel/2), 516)  
-        self.fc1 = torch.nn.Linear(516, 128)
-        self.fc2 = torch.nn.Linear(128, 32)
-        self.fc3 = torch.nn.Linear(32, n_output)
+def param_constraints(params):
+    highest = torch.tensor(10).float()
+    lowest = torch.tensor(-10).float()
+    for index, param in enumerate(params):
+        param = highest if torch.equal(torch.gt(param, highest), torch.tensor(True)) else param
+        param = lowest if torch.equal(torch.lt(param, lowest), torch.tensor(True)) else param
+    return params
 
-    def forward(self, x, n_features, output_channel, batch, input_channel):
-        if(batch < BATCH_SIZE):
-            target = torch.zeros(BATCH_SIZE, input_channel, n_features)
-            target[:batch, :, :] = x
-            x = target
-        x = F.relu(self.conv(x))
-        x = self.pool(x)
-        x = x.view(-1, int(n_features*BATCH_SIZE*output_channel/2))
-        x = F.relu(self.fc(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        x[0][0] = 5 if x[0][0] > 30 or x[0][0] < -30 else x[0][0]
+
+class Net(torch.nn.Module):
+    def __init__(self, n_features, n_output):
+        super(Net, self).__init__()
+        self.fc = torch.nn.Linear(n_features, n_features)  
+        self.fc1 = torch.nn.Linear(n_features, 32)
+        self.fc2 = torch.nn.Linear(n_features, n_output)
+        self.lr = torch.nn.LeakyReLU()
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.lr(x)
+        x = self.fc(x)
+        x = self.lr(x)
+        x = self.fc2(x)
+        '''x[0][0] = 5 if x[0][0] > 30 or x[0][0] < -30 else x[0][0]
         x[0][1] = 5 if x[0][1] > 30 or x[0][1] < -30 else x[0][1]
         x[0][2] = 5 if x[0][2] > 30 or x[0][2] < -30 else x[0][2]
-        x[0][3] = 5 if x[0][3] > 30 or x[0][3] < -30 else x[0][3]
+        x[0][3] = 5 if x[0][3] > 30 or x[0][3] < -30 else x[0][3]'''
+        #print(x)
         return x
 
 
 def loss_func(model, params, pairs):
-    params = params[0]
-    prediction, money = Variable(torch.DoubleTensor([]), requires_grad=True), torch.DoubleTensor([])
+    prediction, money = Variable(torch.FloatTensor([]), requires_grad=True), torch.FloatTensor([])
+    mse = Variable(torch.FloatTensor([]), requires_grad=True)
+    loss = nn.MSELoss()
     if model == 2:
-        for pair in pairs:
-            days = np.array([item[0] for item in pair])
-            prediction = torch.cat((prediction, torch.from_numpy(basic(days, params[0], params[1], params[2], params[3]))), 0)
-            money = torch.cat((money, torch.from_numpy(np.array([item[1] for item in pair])).double()), 0)
+        for index in range(len(params)):
+            pair = pairs[index]
+            param = params.narrow(0, index, 1)
+            days = torch.tensor(np.array([item[0] for item in pair]), requires_grad=True, dtype=torch.float64)
+            prediction = torch.cat((prediction, basic(days, param.narrow(1, 0, 1), param.narrow(1, 1, 1), 
+                param.narrow(1, 2, 1), param.narrow(1, 3, 1)).float()), 0)
+            money = torch.cat((money, torch.from_numpy(np.array([item[1] for item in pair])).float()), 0)
+            mse = torch.cat((mse, torch.FloatTensor([loss(prediction, money).float()])), 0)
 
-        loss = nn.MSELoss()
-        return loss(prediction, money)
+        return torch.mean(mse)
 
 
 torch.manual_seed(1)    
 
 x = pickle.load(open('features.p', 'rb'))  # x data (tensor), shape=(5958, 28)
 y = pickle.load(open('dataset.p', 'rb'))   # y data (tensor), shape=(5958, n, 2)
-x = Variable(torch.from_numpy(np.array(x))).unsqueeze(1).double()     # shape=(5958, 1, 28)
+x = Variable(torch.from_numpy(np.array(x))).float()     # shape=(5958, 28)
 
-net = Net(input_channel=1, output_channel=12, n_features=28, n_output=4, batch=BATCH_SIZE)
+net = Net(n_features=28, n_output=4)
 optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 
 train_dataset = Data.TensorDataset(x[:int(TRAIN_DATASET*5958)])
@@ -199,19 +212,21 @@ fig, ax = plt.subplots(figsize=(16,10))
 
 # start training
 for epoch in range(EPOCH):
-    loader = iter(Data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, num_workers = 0, shuffle=True))
+    loader = iter(Data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, num_workers = 0))   #, shuffle=True
     for step, b_x in enumerate(loader):
         batch = len(b_x[0])
-        b_x = Variable(b_x[0]).float()    # shape=(batch_size, 1,  28)
+        b_x = Variable(b_x[0], requires_grad=True)    # shape=(batch_size, 28)
         b_y = np.array(y[step*BATCH_SIZE : step*BATCH_SIZE+batch])
 
-        params = net(b_x, output_channel=12, n_features=28, batch=batch, input_channel=1)     
-        loss = loss_func(2, params, b_y)     
-
-        optimizer.zero_grad()   
-        loss.backward()         
-        optimizer.step()       
-        if step == 1:
-            print('epoch {}: loss: {}; params: {}'.format(epoch, loss.data.numpy(), params.data.numpy()))
+        params = net(b_x)
+        params.retain_grad()
+        loss = loss_func(2, params, b_y)
+        loss.retain_grad()
+        optimizer.zero_grad()
+        loss.backward()
+        #print(params.grad)
+        optimizer.step()
+        if step == 0:
+            print('epoch {}: loss: {}'.format(epoch, loss.data.numpy()))
 
 
